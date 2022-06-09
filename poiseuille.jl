4550c029bd6d79af6973bf4775798a917676dc85
Bartosz Chaber <bartosz.chaber@ee.pw.edu.pl>
2022-06-09 11:52:16 +0200

    Figured out some of the units

--- poiseuille.jl
+++ poiseuille.jl
@@ -1,26 +1,28 @@
 # physical input parameters
-const ν = 1e-1 # kinematic viscosity [m^2/s]
-const H = 1.00 # channel height [m]
-const ρ = 1e+0 # density [kg/m^3]
-const g = 0.8  # gravity [m/s^2]
+const ν = 0.1 # kinematic viscosity [m^2/s]
+const H = 1.0 # channel height [m]
+const ρ = 1.0 # density [kg/m^3]
+const g = 0.8 # gravity [m/s^2]
 
 # derived parameters
 const um = g * H^2 / (8.0 * ν)    # maximum velocity [m/s]
 const Re = g * H^3 / (8.0 * ν^2)  # Reynolds number [1]
 
 # simulation parameters
-const H̃ = 50   # height of the channel [l. u.]
-const W̃ = 100  # width of the channel [l. u.]
-const ρ̃ = 1.0  # average density [1]
-const τ = 0.8  # relaxation time [1]
+const H̃  = 50  # height of the channel [l. u.]
+const W̃  = 100 # width of the channel [l. u.]
+const ρ̃  = 1.0 # average density [1]
+const τ̃  = 0.8 # relaxation time [1]
+const Δx̃ = 1.0 # assumed (not used)
+const Δt̃ = 1.0 # assumed (not used)
 
 # conversion factors
 CH = H / H̃                      # [m]
 Cρ = ρ / ρ̃                      # [kg / m^3] 
-Ct = (τ - 0.5) / 3.0 * CH^2 / ν # [s]
+Ct = (τ̃ - 0.5) / 3.0 * CH^2 / ν # [s]
 Cu = CH / Ct                    # [m / s]
-Cν = Cu * CH                    # [?]
-Cg = Cρ * CH / Ct^2             # [?]
+Cν = Cu * CH                    # [m^2 / s]
+Cg = Cρ * CH / Ct^2             # [kg / m^3 * m / s^2 = N / m^3]
 
 # normalized parameters
 const g̃  = g / Cg
@@ -28,6 +30,7 @@ const ν̃  = ν / Cν
 const ũm = um / Cu
 const R̃e = ũm * H̃ / ν̃ # normalized Reynolds number matches the physical one
 @assert ũm < 1.0 "Population moves MORE than one cell in a timestep"
+@assert ũm < 0.1 "The results might not be accurate"
 
 using Einsum
 # constants
@@ -42,16 +45,17 @@ const south    = [7, 8, 9] # SE, S, SW
 const weights  = [4/9,1/9,1/36,1/9,1/36,1/9,1/36,1/9,1/36]
 const opposite = [1, 6, 7, 8, 9, 2, 3, 4, 5]
 
+## initial conditions
 # populations on lattice (PDFs)
-const f  = ones(H̃, W̃, Q) / Q # normalized distribution
+const f  = ones(H̃, W̃, Q) * ρ / Q # normalized distribution
 const f′ = similar(f)        # post-collision distribution
-
-# derived quantities on lattice
-const rho = zeros(H̃, W̃)
+# derived, physical quantities on lattice
+const rho = ones(H̃, W̃) * ρ
 const ux  = zeros(H̃, W̃)
 const uy  = zeros(H̃, W̃)
 
 const fi  = zeros(H̃, W̃)
+
 function stream()
     for i in 1:Q
         copyto!(fi,view(f, :, :, i))
@@ -61,9 +65,10 @@ function stream()
     return nothing
 end
 
+const ω̃ = 1.0 / τ̃
 function collide()
     @inbounds for n in 1:H̃, m in 1:W̃
-        vy = τ * g̃ * rho[n, m]
+        vy = τ̃ * g̃ * rho[n, m]
         uu = ux[n, m]^2 + uy[n, m]^2 + vy^2
         
         @inbounds for i in 1:Q
@@ -71,7 +76,7 @@ function collide()
             fi = f[n, m, i]
             feq = rho[n, m] * weights[i] *
                 (1. + 3. * eu + 4.5 * eu^2 - 1.5 * uu)
-            f[n, m, i] -= (1.0 / τ) * (fi - feq)
+            f[n, m, i] -= ω̃ * (fi - feq)
         end
     end
 
